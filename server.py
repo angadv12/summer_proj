083a26e457e6d2a71956fc6a36dc1a2ac4ebd806
angadv12 <angadbrar@outlook.com>
2025-12-28 22:49:46 -0500

    simple zero-shot classifer for task type prediction and confidence (transformers library)

--- server.py
+++ server.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 import uvicorn
+from transformers import pipeline
 
 app = FastAPI()
+
+## brain - zero-shot classifer
+print('Loading zero-shot model...')
+classifer = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+print('Model loaded!')
 
 # 1. normalization layer - incoming notion payload
 def parse_notion_payload(payload: dict):
@@ -41,16 +47,22 @@ async def receive_webhook(payload: dict):
 		print("Received invalid or empty data")
 		return {"status": "ignored"}
 	
+	# zero-shot classification
+	labels = ["Urgent Crisis", "Deep Work", "Administrative Chore", "Relaxation"]
+	ai_result = classifer(clean_data['title'], labels, multi_label=False)
+
+	ai_category = ai_result['labels'][0]
+	ai_confidence = ai_result['scores'][0]
+
+	
 	print("\n--- New Event Detected ---")
 	print(f"Task:   		{clean_data['title']}")
-	print(f"Days Remaining:	{clean_data['days_remaining']}")
 	print(f"Due:    		{clean_data['due_date']}")
-	print(f"Status: 		{clean_data['status']}")
-	print(f"Priority: 		{clean_data['priority']}")
-	print(f"Task Type:   	{clean_data['task_type']}")
+	print(f"Human Priority:	{clean_data['priority']}")
+	print(f"AI Assessment:	{ai_category} ({round(ai_confidence * 100)}% confidence)")
 	print("-----------------------------\n")
 
-	return {"status": "received"}
+	return {"status": "received", "ai_analysis": ai_category}
 
 @app.get("/")
 async def root():
